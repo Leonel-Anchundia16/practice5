f40d243d349359e8e106e02df986574a681fbdf7
unknown <leonelanchundia2000@gmail.com>
2023-07-08 17:23:26 -0500

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'API Dispensador!';
+    return 'API de Dispensador de gasolina!';
   }
 }
