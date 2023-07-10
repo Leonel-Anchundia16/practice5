364c63014ee4d40fe09932a2e6e6efec44b19a12
unknown <leonelanchundia2000@gmail.com>
2023-07-10 10:27:51 -0500

    major: dgsdfgfsdg

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'API de Dispensador con atributo descripcion!';
+    return 'API de Dispensador con atributo !';
   }
 }
